d523bc35a84017ee7ca3f5a35d5ac4204b043798
Randy Coulman <rcoulman@gmail.com>
2015-03-10 23:21:51 -0600

    Move behavior from case statements to Quadrant classes

--- lib/robots/quadrant.rb
+++ lib/robots/quadrant.rb
@@ -1,17 +1,57 @@
 module Quadrant
   class UpperLeft
+    def add_target(board, row, column, target)
+      board.add_target(row, column, target)
+    end
 
+    def add_vertical_wall(board, row, column)
+      board.add_wall_after_column(row, column)
+    end
+
+    def add_horizontal_wall(board, row, column)
+      board.add_wall_after_row(row, column)
+    end
   end
 
   class UpperRight
+    def add_target(board, row, column, target)
+      board.add_target(column, board.size - row - 1, target)
+    end
 
+    def add_vertical_wall(board, row, column)
+      board.add_wall_after_row(column, board.size - row - 1)
+    end
+
+    def add_horizontal_wall(board, row, column)
+      board.add_wall_after_column(column, board.size - row - 2)
+    end
   end
 
   class LowerRight
+    def add_target(board, row, column, target)
+      board.add_target(board.size - row - 1, board.size - column - 1, target)
+    end
 
+    def add_vertical_wall(board, row, column)
+      board.add_wall_after_column(board.size - row - 1, board.size - column - 2)
+    end
+
+    def add_horizontal_wall(board, row, column)
+      board.add_wall_after_row(board.size - row - 2, board.size - column - 1)
+    end
   end
 
   class LowerLeft
+    def add_target(board, row, column, target)
+      board.add_target(board.size - column - 1, row, target)
+    end
 
+    def add_vertical_wall(board, row, column)
+      board.add_wall_after_row(board.size - column - 2, row)
+    end
+
+    def add_horizontal_wall(board, row, column)
+      board.add_wall_after_column(board.size - column - 1, row)
+    end
   end
 end
--- lib/robots/tile.rb
+++ lib/robots/tile.rb
@@ -18,58 +18,19 @@ module Robots
 
     def add_targets(board, quadrant)
       targets.each do |row, column, color, shape|
-        add_target(board, quadrant, row, column, Target.new(color, shape))
+        quadrant.add_target(board, row, column, Target.new(color, shape))
       end
     end
 
     def add_vertical_walls(board, quadrant)
       vertical_walls.each do |row, column|
-        add_vertical_wall(board, quadrant, row, column)
+        quadrant.add_vertical_wall(board, row, column)
       end
     end
 
     def add_horizontal_walls(board, quadrant)
       horizontal_walls.each do |row, column|
-        add_horizontal_wall(board, quadrant, row, column)
-      end
-    end
-
-    def add_target(board, quadrant, row, column, target)
-      case quadrant
-        when Quadrant::UpperLeft
-          board.add_target(row, column, target)
-        when Quadrant::UpperRight
-          board.add_target(column, board.size - row - 1, target)
-        when Quadrant::LowerRight
-          board.add_target(board.size - row - 1, board.size - column - 1, target)
-        when Quadrant::LowerLeft
-          board.add_target(board.size - column - 1, row, target)
-      end
-    end
-
-    def add_vertical_wall(board, quadrant, row, column)
-      case quadrant
-        when Quadrant::UpperLeft
-          board.add_wall_after_column(row, column)
-        when Quadrant::UpperRight
-          board.add_wall_after_row(column, board.size - row - 1)
-        when Quadrant::LowerRight
-          board.add_wall_after_column(board.size - row - 1, board.size - column - 2)
-        when Quadrant::LowerLeft
-          board.add_wall_after_row(board.size - column - 2, row)
-      end
-    end
-
-    def add_horizontal_wall(board, quadrant, row, column)
-      case quadrant
-        when Quadrant::UpperLeft
-          board.add_wall_after_row(row, column)
-        when Quadrant::UpperRight
-          board.add_wall_after_column(column, board.size - row - 2)
-        when Quadrant::LowerRight
-          board.add_wall_after_row(board.size - row - 2, board.size - column - 1)
-        when Quadrant::LowerLeft
-          board.add_wall_after_column(board.size - column - 1, row)
+        quadrant.add_horizontal_wall(board, row, column)
       end
     end
   end
