737af3125ce4d7a0e0b6f96362427b84b8e9862f
Randy Coulman <rcoulman@gmail.com>
2015-03-16 13:05:24 -0700

    Include the new tiles in randomly-generated boards

--- lib/robots/tiles.rb
+++ lib/robots/tiles.rb
@@ -3,7 +3,7 @@ require_relative "tile"
 module Robots
   module Tiles
     def self.groups
-      [[A1, A2], [B1, B2], [C1, C2], [D1, D2]]
+      [[A1, A2, A3, A4], [B1, B2, B3, B4], [C1, C2, C3, C4], [D1, D2, D3, D4]]
     end
 
     def self.random_layout(random)
